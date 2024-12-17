d02ed725370cdad758a233877660e0fd6e8d8ff0
kubaluzak <jakub>
2024-12-17 19:34:17 +0100

    feat : sl4fj added

--- src/main/java/pl/put/poznan/sorter/app/SortingMadnessApplication.java
+++ src/main/java/pl/put/poznan/sorter/app/SortingMadnessApplication.java
@@ -1,13 +1,15 @@
 package pl.put.poznan.sorter.app;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+@Slf4j
 @SpringBootApplication(scanBasePackages = {"pl.put.poznan.sorter"})
 public class SortingMadnessApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SortingMadnessApplication.class, args);
+        log.warn("App started (SL4FJ)");
     }
 }
